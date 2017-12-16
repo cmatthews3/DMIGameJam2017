95ed4d994cf3adf6ebbcce2e694225628777f9bd
Chris Matthews <chris.matthews@lan.iinfosec.net>
2017-12-16 17:25:40 -0600

    commited again

---
+++ Assets/Scripts/InputReferenceManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Movement
+{
+    public KeyCode up;
+    public KeyCode down;
+    public KeyCode left;
+    public KeyCode right;
+}
+
+
+public class InputReferenceManager : MonoBehaviour {
+
+    public static InputReferenceManager instance;
+
+    public Movement movement;
+
+    // Use this for initialization
+    private void Awake()
+    {
+        if (instance == null) instance = this;
+    }
+
+
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+
+    Rigidbody2D body;
+
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(InputReferenceManager.instance.movement.up))
+        {
+            transform.position += transform.up * speed;
+            transform.eulerAngles = Vector3.zero;
+        }
+        if (Input.GetKey(InputReferenceManager.instance.movement.down))
+        {
+            transform.position -= transform.up * speed;
+            transform.eulerAngles = new Vector3(0,0,180);
+        }
+        else
+        {
+            body.velocity = Vector3.zero;
+        }
+    }
+}
